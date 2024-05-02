c617c3b59b6c99dd0a504e6989889b2869161153
Vijaya <vijayarubini@gmail.com>
2024-05-02 23:48:21 +0530

    Initial commit

---
+++ src/ex_02052024/lab125.java
@@ -0,0 +1,15 @@
+package ex_02052024;
+
+public class lab125 {
+    public static void main(String[] args) {
+
+        int[] a=new int[4]; // -> size- 4, index= 0-3
+        //default a=[0,0,0,0]
+        //JVM- |0|0|0|0|
+       // a[4]=70;ArrayIndexOutOfBoundException
+      //  a[5]=80;
+
+
+
+    }
+}
---
+++ src/ex_02052024/lab134.java
@@ -0,0 +1,9 @@
+package ex_02052024;
+
+public class lab134 {
+    public static void main(String[] args) {
+
+        int[] a={1,2,3};
+        System.out.println(a[10]); // arrayIndexOutofBoundException
+    }
+}
