a5ae4468ff53c6e52474506fcbfe3b7c1e705c2c
VijayaRubini <vijayarubini@gmail.com>
2024-05-11 12:39:17 +0530

    commiting all deleted files

--- src/ex_11042024/lab001.java
+++
@@ -1,8 +0,0 @@
-package ex_11042024;
-
-public class lab001 {
-    public static void main(String[] args) {
-        //single line comment
-        System.out.println("Hello world");
-    }
-}
--- src/ex_20042024/lab050.java
+++
@@ -1,9 +0,0 @@
-package ex_20042024;
-
-public class lab050 {
-    public static void main(String[] args) {
-        byte b=10;
-        int a=b; //implicit casting -JVM
-        int a1=(int) b; //explicit casting
-    }
-}
